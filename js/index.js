6fbdc73d05b8eb54b48c2b944085478f1fc31f5b
Syrax <teagankeith@outlook.com>
2020-04-07 21:41:22 -0400

    Fixed images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,14 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src = "img/logo.png"
+
+
+let ctaimg = document.getElementById("cta-img");
+ctaimg.src = "img/header-img.png"
+
+let middleimg = document.getElementById("middle-img");
+
+middleimg.src = "img/mid-page-accent.jpg"
+
+
